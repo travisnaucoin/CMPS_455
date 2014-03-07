fbfa7b90c4f63f6e4f6f700a5a19c88a28fed1e9
anderson1008 <anderson.xxy@gmail.com>
2014-03-07 15:23:39 -0600

    exec.c
    change ../test/exec.c

--- Nachos/nachos-3.4/code/test/exec.c
+++ Nachos/nachos-3.4/code/test/exec.c
@@ -1,14 +1,16 @@
-// test exec()
+/ test exec()
 
 #include "syscall.h"
 
 int main() {
 
 	int FileId1;
-	FileId2 = Exec("../test/test1");
+	int FileId2;
+	
+	FileId1 = Exec("../test/test1");
 	Join(FileId1);
 
-	int FileId2;
+	
 	FileId2 = Exec("../test/halt");
 	Join(FileId2);
 	//Halt();
